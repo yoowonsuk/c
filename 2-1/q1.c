d82f8e19d59463b2d9350a622498a4a85d897eaa
yoowonsuk <sam2yuk@naver.com>
2019-07-06 18:17:55 +0900

    6th july 2019

---
+++ 2-1/q1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("HongGilDong\nHong Gil Dong\nHong  Gil  Dong\n");
+	return 0;
+}
---
+++ 2-1/q2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Name: Yoo Won Suk\n");
+	printf("Address: Gangnam-gu Seoul Korea\n");
+	printf("Number: 010-1234-5678\n");
+	return 0;
+}
